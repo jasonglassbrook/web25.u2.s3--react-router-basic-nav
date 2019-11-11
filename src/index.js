8353ffb8f705a8d5d850e318102e27695845b94a
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-11 17:06:36 -0500

    comments ; reorder imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+/// external modules ///
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
 
+/// intenal modules ///
+import App from './App';
+
+/// styles ///
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+
+/***********************************************************
+  RENDER
+***********************************************************/
 ReactDOM.render(<App />, document.getElementById('root'));
