aae16b58f3e05694b6bf49525f7fe20ab2041f52
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-11 17:18:41 -0500

    import Route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 /// external modules ///
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 /// internal modules ///
 import { Home, About, Contact, Navigation } from './components';
